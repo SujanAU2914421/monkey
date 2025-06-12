4358e9fdc4578fd4af32058491b570ba67b97b05
SujanAU2914421 <deecoodeer@gmail.com>
2025-06-12 14:39:36 +0545

    main update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,42 +1,47 @@
 import Alpine from "alpinejs";
 
 Alpine.store("typer", {
-    mainTexts: "Here we go again!",
+    mainTexts:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     textsInput: "",
 
-    textBeingRendered: "",
+    mainTextsArrayFormat: [],
+    arrayOfTextsInput: [],
     arrayOfTextBeingRendered: [],
 
+    challengeNotComplete: true,
     challengeStarted: false,
 
-    get mainTextWordsArray() {
-        return this.mainTexts.trim().split(/\s+/);
-    },
+    givenTime: 5,
+    timeShower: 0,
+    accuracy: 0,
+    wordsPerMinute: 0,
 
     init() {
-        this.textBeingRendered = this.mainTexts;
-        this.arrayOfTextBeingRendered = this.mainTextWordsArray;
-    },
+        this.mainTextsArrayFormat = this.mainTexts.trim().split(/\s+/);
+        this.arrayOfTextBeingRendered = [...this.mainTextsArrayFormat];
 
-    startChallenge() {
-        this.challengeStarted = true;
-        console.log("the challenge");
+        window.addEventListener("keydown", (e) => {
+            const blockedKeys = [
+                "ArrowLeft",
+                "ArrowRight",
+                "ArrowUp",
+                "ArrowDown",
+                "Tab",
+                "Enter",
+            ];
 
-        // const oneSecInterval = setInterval(() => {
-        //     console.log("interval");
-        // }, 1000);
-
-        setTimeout(() => {
-            // clearInterval(oneSecInterval);
-            this.challengeStarted = false;
-            console.log("challenge ended");
-        }, 3000);
+            if (blockedKeys.includes(e.key)) {
+                e.preventDefault();
+            }
+        });
     },
 
     inputChangeHandler(e) {
-        if (!this.challengeStarted) {
-            this.startChallenge;
+        if (!this.challengeStarted && this.challengeNotComplete) {
+            timer(this.challengeNotComplete, this.givenTime, this.timeShower);
         }
+        this.challengeStarted = true;
 
         let value = e.target.value;
 
@@ -44,34 +49,90 @@ Alpine.store("typer", {
         e.target.value = value;
 
         this.textsInput = value;
-        this.updateTextBeingRendered();
+        this.arrayOfTextsInput = this.textsInput.trim().split(/\s+/);
+
+        let temp = this.arrayOfTextsInput.map((val, i) => {
+            const original = this.mainTextsArrayFormat[i] || "";
+            return smartMerge(val, original);
+        });
+
+        this.arrayOfTextBeingRendered = mergeMain(
+            temp,
+            this.mainTextsArrayFormat
+        );
     },
 
-    updateTextBeingRendered() {
-        const inputWords = this.textsInput.trim().split(/\s+/);
-        const mainWords = this.mainTextWordsArray;
+    letterChecker(wordIndex, letterIndex) {
+        const userWord = this.arrayOfTextsInput[wordIndex];
+        const mainWord = this.mainTextsArrayFormat[wordIndex];
 
-        if (inputWords != "") {
-            // Build the rendered text
-            const renderedWords = [];
+        if (!mainWord) return "text-gray-600";
+        if (!userWord) return "text-gray-600";
 
-            // Push typed words first
-            for (let i = 0; i < inputWords.length; i++) {
-                renderedWords.push(inputWords[i]);
-            }
+        if (userWord[letterIndex] === undefined) return "text-gray-600";
 
-            // Push remaining mainTexts words that are not yet typed
-            for (let i = inputWords.length; i < mainWords.length; i++) {
-                renderedWords.push(mainWords[i]);
-            }
-
-            this.textBeingRendered = renderedWords.join(" ");
-            this.arrayOfTextBeingRendered = renderedWords;
-        } else {
-            this.arrayOfTextBeingRendered = this.mainTexts.trim().split(/\s+/);
-        }
-        return this.arrayOfTextBeingRendered;
+        return userWord[letterIndex] === mainWord[letterIndex]
+            ? "text-white"
+            : "text-red-500";
     },
+    restart() {},
 });
+
+function mergeMain(mergedUserInput, mainTextsArrayFormat) {
+    let temp = [];
+    for (let i = 0; i < mainTextsArrayFormat.length; i++) {
+        if (mergedUserInput[i] != undefined) {
+            temp[i] = mergedUserInput[i];
+        } else {
+            temp[i] = mainTextsArrayFormat[i];
+        }
+    }
+    return temp;
+}
+
+function smartMerge(userInput, original) {
+    if (!userInput) return original;
+
+    userInput = userInput;
+    original = original;
+
+    for (let i = userInput.length; i >= 0; i--) {
+        return userInput + original.slice(i);
+    }
+
+    return original;
+}
+
+function timer() {
+    let store = Alpine.store("typer");
+
+    store.timeShower = store.givenTime;
+
+    let counterInterval = setInterval(() => {
+        store.timeShower--;
+
+        if (store.timeShower <= 0) {
+            clearInterval(counterInterval);
+
+            scoreChecker();
+
+            store.challengeNotComplete = false;
+        }
+    }, 1000);
+}
+
+function scoreChecker() {
+    let store = Alpine.store("typer");
+    store.wordsPerMinute =
+        store.arrayOfTextsInput.length / (store.givenTime / 60);
+
+    let count = 0;
+    for (let i = 0; i < store.arrayOfTextsInput.length; i++) {
+        if (store.arrayOfTextsInput[i] === store.mainTextsArrayFormat[i]) {
+            count++;
+        }
+    }
+    store.accuracy = (count / store.arrayOfTextsInput.length) * 100;
+}
 
 Alpine.start();
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,37 +9,65 @@
 </head>
 
 <body>
-    <div x-data class="relative min-h-screen w-screen flex items-center justify-center">
+    <div x-data="{ timerCount: 0 }" class="relative min-h-screen w-screen flex items-center justify-center">
+        <div class="fixed top-0 right-0 z-10 h-20 w-20 flex items-center justify-center font-bold text-3xl text-white"
+            x-init="$watch(() => $store.typer.timeShower, value => timerCount = value)" x-text="timerCount">
+        </div>
         <div class="relative w-[900px] px-4">
             <div class="text-5xl text-gray-200 uppercase font-mono font-bold text-center mb-8">
                 Monkey Clone
             </div>
-
-            <!-- Rendered Typing Area -->
-            <div class="text-3xl font-bold text-white flex flex-wrap gap-2">
-                <template x-for="(word, wordIndex) in $store.typer.updateTextBeingRendered()" :key="wordIndex">
-                    <div class="flex gap-1">
-                        <template x-for="(letter, letterIndex) in word.split('')" :key="letterIndex">
-                            <div x-text="letter"
-                                :class="letter === $store.typer.mainTextWordsArray[wordIndex]?.[letterIndex] ?
-                                    'text-white' :
-                                    'text-red-500'">
+            <template x-if="$store.typer.challengeNotComplete">
+                <div class="relative">
+                    <div class="relative flex gap-3 justify-center flex-wrap">
+                        <template x-for="(oneWord, wordIndex) in $store.typer.arrayOfTextBeingRendered">
+                            <div class="relative flex gap-[2px]">
+                                <template x-for="(oneLetter, letterIndex) in oneWord">
+                                    <div x-text="oneLetter" :class="$store.typer.letterChecker(wordIndex, letterIndex)"
+                                        class="font-bold text-2xl"></div>
+                                </template>
                             </div>
                         </template>
                     </div>
-                </template>
-            </div>
 
-            <!-- Typing Input -->
-            <div class="mt-8">
-                <input autofocus type="text" class="w-full text-white max-w-md px-4 py-2 rounded-md"
-                    :value="$store.typer.textsInput" @input="$store.typer.inputChangeHandler($event)"
-                    placeholder="Type here..." />
-                <div class="flex">
-
-                    <div class="text-white font-bold mt-3">Type anything to start challenge</div>
+                    <div class="mt-8">
+                        <div class="relative flex justify-center">
+                            <input autofocus type="text"
+                                class="w-full outline-none border text-white max-w-md px-4 py-2 rounded-md"
+                                :value="$store.typer.textsInput" @input="$store.typer.inputChangeHandler($event)"
+                                placeholder="Type here..." />
+                        </div>
+                        <div class="flex justify-center">
+                            <div class="text-white font-bold mt-3">Type to start challenge</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </template>
+            <template x-if="!$store.typer.challengeNotComplete">
+                <div class="relative flex justify-center">
+                    <div class="relative">
+                        <div class="relative flex items-center justify-center gap-8">
+                            <div class="relative">
+                                <div class="relative text-xl text-gray-300">Accuracy</div>
+                                <div class="relative text-white font-bold text-3xl mt-4">
+                                    <span x-text="$store.typer.accuracy"></span>%
+                                </div>
+                            </div>
+                            <div class="relative">
+                                <div class="relative text-xl text-gray-300">WPM</div>
+                                <div class="relative text-white font-bold text-3xl mt-4">
+                                    <span x-text="$store.typer.wordsPerMinute"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="relative flex justify-center mt-8">
+                            <div onclick="window.location.reload()"
+                                class="h-10 px-8 rounded-md bg-white cursor-pointer text-gray-800 font-bold text-sm flex items-center justify-center">
+                                Re Start?</div>
+                        </div>
+                    </div>
+                </div>
+            </template>
         </div>
     </div>
 </body>
